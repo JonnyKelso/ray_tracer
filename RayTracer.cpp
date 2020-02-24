b135e42d8438f36d3988b01aa36ebe479e381631
Jonny Kelso <JonnyKelso@users.noreply.github.com>
2020-02-24 20:30:46 +0000

    Now renders a sphere. New Hittable class

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,17 +1,49 @@
 //#include "vec3.h"
-#include "ray.h"
+//#include "ray.h"
+#include "limits.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
 #include <iostream>
 
 //#define STB_IMAGE_IMPLEMENTATION
 //#include <stb_image.h>
+vec3 colour(const ray& r, hittable *world);
+float hit_spehre(const vec3& centre, float radius, const ray& r);
 
-vec3 colour(const ray& r)
+
+vec3 colour(const ray& r, hittable *world)
 {
-    vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y() + 1.0);
-    return ( ((1.0 - t) * vec3(1.0, 1.0, 1.0)) + (t * vec3(0.5, 0.7, 1.0)) );
+    hit_record rec;
+    if(world->hit(r, 0.0, std::numeric_limits<float>::max(), rec))
+    {
+        return (0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1));
+    }
+    else
+    {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        return ( ((1.0 - t) * vec3(1.0, 1.0, 1.0)) + (t * vec3(0.5, 0.7, 1.0)) );
+    }
 }
 
+/*float hit_spehre(const vec3& centre, float radius, const ray& r)
+{
+    // good old (-b +/- sprt(b^2 - 4ac)) / 2a
+    vec3 oc = r.origin() - centre;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}*/
 
 int main()
 {
@@ -24,6 +56,12 @@ int main()
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
 
+    hittable *list[2];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hittable *world = new hittable_list(list, 2);
+
+
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
@@ -31,9 +69,10 @@ int main()
 
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
-
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            vec3 col = colour(r);
+            
+            vec3 p = r.point_at_parameter(2.0);
+            vec3 col = colour(r, world);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
---
+++ build_run.bat
@@ -0,0 +1,3 @@
+
+call build.bat
+bin\RayTracer.exe > images\img.ppm
